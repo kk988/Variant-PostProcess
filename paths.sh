13cea5a980b6a1fe1c3d0f0a8a995937922ac39b
Krista Kazmierkiewicz <kristakaz@s01.cbio.private>
2015-09-23 12:34:23 -0400

    Merging soccins changes
    Maybe this will fix pr/2

--- paths.sh
+++ paths.sh
@@ -2,9 +2,8 @@
 BEDTOOLS=/opt/common/CentOS_6/bedtools/bedtools-2.22.0/bin/bedtools
 VEPPATH=/opt/common/CentOS_6/vep/v81
 PERL=/opt/common/CentOS_6-dev/perl/perl-5.22.0/bin/perl
-VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.1
-MSK_ISOFORMS=/opt/common/CentOS_6-dev/vcf2maf/v1.6.1/data/isoform_overrides_at_mskcc
 PYTHON_PATH=/opt/common/CentOS_6/python/python-2.7.8/bin
 TABIX_PATH=/opt/common/CentOS_6/samtools/samtools-1.2/htslib-1.2.1
 SAMTOOLS_PATH=/opt/common/CentOS_6/samtools/samtools-1.2
-
+VCF2MAF=/opt/common/CentOS_6/vcf2maf/v1.6.2
+MSK_ISOFORMS=$VCF2MAF/data/isoform_overrides_at_mskcc
